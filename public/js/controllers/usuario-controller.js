3d2e0402b953c73ec35bbef25ff36afe5183ba58
Kelvin Feitosa <kelvinwnfeitosa@gmail.com>
2017-06-02 19:38:09 -0300

    Alterações do Usuário

--- public/js/controllers/usuario-controller.js
+++ public/js/controllers/usuario-controller.js
@@ -1,33 +1,33 @@
 angular.module('limoservice')
-    .controller('UsuarioController', function($http) {
+    .controller('UsuarioController', function ($http) {
 
-    var vm = this;
+        var vm = this;
 
-    vm.usuario = {};
+        vm.usuario = {};
 
-    vm.iniciar = iniciar;
-    vm.limpar = limpar;
-    vm.cadastrar = cadastrar;
+        vm.iniciar = iniciar;
+        vm.limpar = limpar;
+        vm.cadastrar = cadastrar;
 
-    function iniciar() {
-        vm.usuario.login = '';
-        vm.usuario.senha = '';
-        vm.usuario.admin = false;
-        $('#login').focus();
-    };
+        function iniciar() {
+            vm.usuario.login = '';
+            vm.usuario.senha = '';
+            vm.usuario.admin = false;
+            $('#login').focus();
+        };
 
-    function limpar() {
-        iniciar();
-        vm.loginForm.$setPristine();
-    };
+        function limpar() {
+            iniciar();
+            vm.loginForm.$setPristine();
+        };
 
-    function cadastrar() {
-        $http.post('/v1/usuarios', angular.toJson(vm.usuario))
-            .then(function(usuario) {
-            console.log(usuario);
-        }, function(error) {
-            console.log(error);
-        });
-    };
+        function cadastrar() {
+            $http.post('/v1/usuarios', angular.toJson(vm.usuario))
+                .then(function (usuario) {
+                    console.log(usuario);
+                }, function (error) {
+                    console.log(error);
+                });
+        };
 
-});
+    });
